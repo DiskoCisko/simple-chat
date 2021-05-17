ea8b9739c02f573979f01d422ac89891228525d1
DiskoCisko <53272759+DiskoCisko@users.noreply.github.com>
2021-05-17 09:51:09 +0300

    fix css

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App/App'
 
-import './index.scss'
+import './index.css'
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
